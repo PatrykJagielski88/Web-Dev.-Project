49359131995fe5d12bf4c250c7af796fe7171433
PatrykJagielski88 <92446604+PatrykJagielski88@users.noreply.github.com>
2021-11-21 15:30:40 -0600

    Add files via upload
    Added functionality for logged in users. Now a picture can be uploaded when creating or updating a post. Foxed some minor problems.

--- edit.php
+++ edit.php
@@ -3,6 +3,11 @@
 
   require('connect.php');
   require('authenticate.php');
+
+  include ('ImageResize.php');
+  include ('ImageResizeException.php');
+
+  use \Gumlet\ImageResize;
   // I have the ID for the post that I need to get
 
   if (!empty($_GET['postId']) && is_numeric($_GET['postId']) && filter_input(INPUT_GET, "postId", FILTER_SANITIZE_NUMBER_INT)) {
@@ -17,12 +22,133 @@
     $fetch_statement->execute();
   
     $row = $fetch_statement->fetch();
+    
 
+  
+
+}
+if (isset($_POST)) {
+  if($_POST && !empty($_POST['title']) && !empty($_POST['content'])&& is_numeric($_POST['postId']) 
+    && filter_input(INPUT_POST, "postId", FILTER_SANITIZE_NUMBER_INT)) {
+
+  // First we need to sanitize our input first
+  $sanitized_post = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+  // prepare the query that we'll want to use
+  $insert_query = "UPDATE post SET title=:title, content=:content WHERE postId=:postId LIMIT 1";
+
+  // Prepare the Database Object with the query
+  $statement = $db->prepare($insert_query); // Returns a PDOStatement object.
+
+  // bind our values to our placeholders
+  $statement->bindValue(':title', $sanitized_post['title']);
+  $statement->bindValue(':content', $sanitized_post['content']);
+  $statement->bindValue(':postId', $sanitized_post['postId']);
+  // $statement->bindValue(':categoryId', $sanitized_post['categoryId']);
+
+  // Finally execute the query
+  $statement->execute(); // The query is now executed.
+
+    if ($statement->rowCount() >= 0) {
+        header('Location: index.php');
+        exit();
+    }
+  }
+  elseif ($_POST && empty($_POST['title']) && empty($_POST['content'])) {
+    exit("The title and content can NOT be empty.");
+  }
+
+  if ($_POST && $_POST['submit'] == 'Upload Image') {
+
+       
+    // $query2 = "SELECT LAST_INSERT_ID()";
+    // $statement2 = $db->prepare($query2); // Returns a PDOStatement object.
+    // $statement2->execute(); // The query is now executed.
+    
+    // $row2 = $statement2->fetch();
+    // print_r($row2);
+ 
+     $insert_query2 = "INSERT INTO images (imageName, postId) VALUES (:imageName, :postId)";
+ 
+     // Prepare the Database Object with the query
+     $statement2 = $db->prepare($insert_query2); // Returns a PDOStatement object.
+   
+     // bind our values to our placeholders
+     $statement2->bindValue(':imageName', $_FILES['uploaded_file']['name'] );
+     $statement2->bindValue(':postId', $_POST["postId"]);
+   
+     // Finally execute the query
+     $statement2->execute(); // The query is now executed.
+ 
    }
-   else {
-    header('Location: index.php');
-    exit();
-   }
+}
+
+  print_r('hellolsdkjhs');
+ print_r($_POST);
+
+
+
+
+
+
+
+// file_upload_path() - Safely build a path String that uses slashes appropriate for our OS.
+// Default upload path is an 'uploads' sub-folder in the current folder.
+function file_upload_path($original_filename, $upload_subfolder_name = 'uploads') {
+   $current_folder = dirname(__FILE__);
+   
+   // Build an array of paths segment names to be joins using OS specific slashes.
+   $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+   
+   // The DIRECTORY_SEPARATOR constant is OS specific.
+   return join(DIRECTORY_SEPARATOR, $path_segments);
+}
+// print_r("current folder".$current_folder);
+// file_is_valid() - Checks the mime-type & extension of the uploaded file for "image-ness".
+function file_is_valid($temporary_path, $new_path) {
+    $allowed_mime_types      = ['image/gif', 'image/jpeg', 'image/png'];
+    $allowed_file_extensions = ['gif', 'jpg', 'jpeg', 'png'];
+    
+    $actual_file_extension   = pathinfo($new_path, PATHINFO_EXTENSION);
+    $actual_mime_type        = mime_content_type($temporary_path);
+    
+    $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+    $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
+    
+    return $file_extension_is_valid && $mime_type_is_valid;
+}
+
+$upload_detected = isset($_FILES['uploaded_file']) && ($_FILES['uploaded_file']['error'] === 0);
+$upload_error_detected = isset($_FILES['uploaded_file']) && ($_FILES['uploaded_file']['error'] > 0);
+
+if ($upload_detected) { 
+    $filename        = $_FILES['uploaded_file']['name'];
+    $temporary_path  = $_FILES['uploaded_file']['tmp_name'];
+    $new_path        = file_upload_path($filename);
+    print_r($filename);
+    if (file_is_valid($temporary_path, $new_path)) { 
+        $actual_file_extension = pathinfo($new_path, PATHINFO_EXTENSION);
+
+        $path = basename($new_path,$actual_file_extension);
+print_r("path ".$path);
+
+        move_uploaded_file($temporary_path, $new_path);
+        
+        $actual_file_name = pathinfo($new_path, PATHINFO_FILENAME);
+        if ($actual_file_extension !== 'pdf') {
+            $image_medium = new ImageResize("{$new_path}");
+            $image_medium->resizeToLongSide(150);
+            $image_medium->save('uploads'.DIRECTORY_SEPARATOR."{$actual_file_name}_medium.{$actual_file_extension}");
+
+        //     $image_thumbnail = new ImageResize("{$new_path}");
+        //     $image_thumbnail->resizeToLongSide(50);
+        //     $image_thumbnail->save('uploads'.DIRECTORY_SEPARATOR."{$actual_file_name}_thumbnail.{$actual_file_extension}");
+
+          rename('uploads'.DIRECTORY_SEPARATOR."{$actual_file_name}.{$actual_file_extension}", 'uploads'.DIRECTORY_SEPARATOR."{$actual_file_name}_original.{$actual_file_extension}");
+          rename('uploads'.DIRECTORY_SEPARATOR."{$actual_file_name}_medium.{$actual_file_extension}", 'uploads'.DIRECTORY_SEPARATOR."{$actual_file_name}.{$actual_file_extension}");
+        }                    
+    }
+}
 
 ?>
 
@@ -44,7 +170,7 @@
           <li><a href="create.php" >New Post</a></li>
       </ul> 
       <div id="all_blogs">
-        <form action="update.php" method="post">
+        <form action="edit.php" method="post" enctype='multipart/form-data'>
           <fieldset>
             <legend>Edit Blog Post</legend>
             <p>
@@ -61,7 +187,18 @@
               <input type="submit" formaction="delete.php?postId=<?= $row['postId'] ?>" value="Delete">
             </p>
           </fieldset>
+          <label for='uploaded_file'>Image Filename:</label>
+          <input type='file' name='uploaded_file' id='uploaded_file'>
+          <input type='submit' name='submit' value='Upload Image'>
         </form>        
+        <?php if ($upload_error_detected): ?>
+          <p>Error Number: <?= $_FILES['uploaded_file']['error'] ?></p>
+        <?php elseif ($upload_detected): ?>
+          <p>Client-Side Filename: <?= $_FILES['uploaded_file']['name'] ?></p>
+          <p>Apparent Mime Type:   <?= $_FILES['uploaded_file']['type'] ?></p>
+          <p>Size in Bytes:        <?= $_FILES['uploaded_file']['size'] ?></p>
+          <p>Temporary Path:       <?= $_FILES['uploaded_file']['tmp_name'] ?></p>
+        <?php endif ?>
       </div>
           <div id="footer">
               Copywrong 2021 - No Rights Reserved
