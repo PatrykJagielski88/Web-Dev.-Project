560eee9bad2e24a7b63f8daa967a6e80cc561cc3
PatrykJagielski88 <92446604+PatrykJagielski88@users.noreply.github.com>
2021-12-01 10:51:26 -0600

    Add files via upload
    Modified some elements of the page to better fit my design and fixed minor problems. Now only logged in users can perform a search.

--- php-database-search.php
+++ php-database-search.php
@@ -1,54 +1,53 @@
 <?php
-require('connect.php');
+    session_start();
+    require('connect.php');
+    require('authenticate.php');
+
+    if ($_POST && filter_input(INPUT_POST, 'search', FILTER_SANITIZE_SPECIAL_CHARS)) {
+        $search=$_POST['search'];
+        $query = $db->prepare("select * from post where Title LIKE '{$_POST['search']}' OR content LIKE '{$_POST['search']}'");
+        $query->execute();
+    }
+    
 ?>
 
 
 <html>
 <head>
 <title>Search functionality in dynamic website using php, MySQL | Technopoints</title>
-<link rel="stylesheet" href="css/w3.css">
+<link rel="stylesheet" href="styles.css" type="text/css">
 </head>
 <body>
-<div class="w3-panel w3-pink">
-  <h1 class="w3-opacity">
-  <b>Search Functionality using php, MySQL</b></h1>
-</div>
-<?php 
-//load database connection
-    // $host = "localhost";
-    // $user = "root";
-    // $password = "";
-    // $database_name = "search";
-    // $db = new PDO("mysql:host=$host;dbname=$database_name", $user, $password, array(
-    // PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    // ));
-// Search from MySQL database table
-$search=$_POST['search'];
-$query = $db->prepare("select * from post where Title LIKE '{$_POST['search']}' OR content LIKE '{$_POST['search']}'");
-//$query->bindValue("%$search%", PDO::PARAM_STR);
-$query->execute();
-
-print_r($_POST['search']);
-// Display search result
- 
-         if (!$query->rowCount() == 0) {
-		 		echo "Search found :<br/>";
-				echo "<table class='w3-table-all'>";	
-                echo "<tr class='w3-red'><td>Employee name</td><td>Email</td><td>Company</td></tr>";				
-            while ($results = $query->fetch()) {
-				echo "<tr><td>";			
-                echo $results['title'];
-				echo "</td><td>";
-                echo $results['postId'];
-				echo "</td><td>";
-                echo $results['content'];
-				echo "</td></tr>";				
-            }
-				echo "</table>";		
-        } else {
-            echo 'No results found!';
-        }
-?>
+<div id="wrapper">
+        <div id="header">
+            <h1><a href="index.php">"Polish Pat's potatoe pancakes"</a></h1>
+        </div> 
+    <ul id="menu">
+        <li><a href="index.php" >Home</a></li>
+        <li><a href="create.php" class='active'>New Post</a></li>
+    </ul> 
+<div id="all_blogs">
+<?php if (isset($_SESSION["loggedin"])):?>
+    <?php if (!empty($query) && !$query->rowCount() == 0): ?>
+        Your search <?= ($_POST['search']) ?> found :<br/> 
+        <?php foreach($query as $row): ?> 
+            
+            <table class="blog_post">
+            <h2><a href="show.php?postId=<?= $row["postId"] ?>"><?= $row['title'] ?></a></h2>
+            PostId:<?= $row["postId"] ?></br>                    
+            Date:<?= $row["date"] ?></br>
+            
+            <!-- Category name:<?= $row['categoryName'] ?>       </br> -->
+            <div class='blog_content'>
+                <!--  -->
+                    <p>Content:<?= $row["content"] ?></p>          
+                    <p>UserId:<?= $row["userId"] ?></p>
+                    <a href="edit.php?postId=<?= $row["postId"] ?>">edit</a>
+                <!--  -->
+            </div>               
+        <?php endforeach; ?> 
+    <?php endif; ?>
+<?php endif; ?>
 </div>
 
 </body>
